2a740e2b99431ef1f24357c0263f06b89a508035
Alvaro Barba <abarba.lod@gmail.com>
2023-05-15 19:34:38 +0200

    sin cambios

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,10 +5,10 @@ const router = express.Router();
 /* GET home page */
 router.get("/profile", (req, res, next) => {
 
-  const {_id} = req.session.currentUser
+  const { _id } = req.session.currentUser
 
   User.findById(_id)
-  .then(user => res.render("users/profile" , user))
+    .then(user => res.render("users/profile", user))
 
 });
 
