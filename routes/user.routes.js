0051fb6c0370ee309b9dfe17900adc966bfe39fa
sergiolopez <sergio.lopez.souto@gmail.com>
2023-05-16 11:06:35 +0200

    solving problems

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -10,17 +10,12 @@ router.get("/profile", (req, res, next) => {
 
   const { _id } = req.session.currentUser
 
-  User.findById(_id)
-    .then(user => res.render("users/profile", user))
   Event.find({ assistants: { $in: _id } })
     .then(eventList => {
       User.findById(_id)
         .then(user => res.render("users/profile", { user, eventList }))
     })
     .catch(err => next(err))
-
-  // User.findById(_id)
-  // .then(user => res.render("users/profile" , user))
 
 });
 
